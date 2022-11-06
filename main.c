103142d7598d185cf598df14fb51c19c53a769a1
Cing-Chen <yuxiang.hong.tw@gmail.com>
2022-11-07 04:01:54 +0800

    Finish the items that we need to check

--- main.c
+++ main.c
@@ -4,41 +4,51 @@
 // #include <unistd.h>
 
 
-void* pt_func_1(void) {
-    // for(int i = 0; i < 5; i++) {
-    //     printf("This is pthread 1 and i = %d.\n", i);
-    //     sleep(1);
-    // }
-
-    // Write thread 1 code here.
-
-    pthread_exit(NULL);
-}
+typedef struct {
+    char *segment_name;
+    void *address;
+} data_format;
 
 
-void* pt_func_2(void) {
-    // for(int i = 0; i < 5; i++) {
-    //     printf("This is pthread 2 and i = %d.\n", i);
-    //     sleep(1);
-    // }
+int for_data_segment = 48763;
+int for_bss_segment = 0;
+int *for_heap_segment;
+void *for_libraries_segment = &printf;
+void for_text_segment() {}
 
-    // Write thread 2 code here.
+
+void* child_thread(void) {
+    int for_stack_segment = 357;
+
+    data_format items[] = {
+        {"text", &for_text_segment}
+        , {"data", &for_data_segment}
+        , {"bss", &for_bss_segment}
+        , {"heap", for_heap_segment}
+        , {"libraries", for_libraries_segment}
+        , {"stack", &for_stack_segment}
+    };
+
+    for(int i = 0; i < (sizeof(items)/sizeof(items[0])); i++) {
+        printf("Segment Name: %s, Address: %p\n"
+            , items[i].segment_name
+            , items[i].address
+        );
+    }
 
     pthread_exit(NULL);
 }
 
 
 int main(void) {
+    for_heap_segment = malloc(10);
+    for_libraries_segment = &printf;
+
     pthread_t pt_1 = 0;
     pthread_t pt_2 = 0;
 
-    pthread_create(&pt_1, NULL, (void*)pt_func_1, "pt_func_1");
-    pthread_create(&pt_2, NULL, (void*)pt_func_2, "pt_func_2");
-
-    // for(int i = 0; i < 5; i++) {
-    //     printf("This is main thread and i = %d\n", i);
-    //     sleep(1);
-    // }
+    pthread_create(&pt_1, NULL, (void*)child_thread, "pt_1");
+    pthread_create(&pt_2, NULL, (void*)child_thread, "pt_2");
 
     pthread_join(pt_1, NULL);
     pthread_join(pt_2, NULL);
